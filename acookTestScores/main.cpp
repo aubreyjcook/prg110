d07b57af1a635a67fcff3f87bae02765e07c31a6
aubreyjcook <aubreyjcook@gmail.com>
2016-05-02 11:21:18 -0500

    TestScores update001 05022016
    arrays allocation fixed
    descending bubble sort added

--- acookTestScores/main.cpp
+++ acookTestScores/main.cpp
@@ -2,22 +2,20 @@
 
 using namespace std;
 
-const int CAPACITY = 100;
-const int TESTSCOREMAX = 1000;
+void sortArray(int array[], int size);
+
 int main()
 {
-    //creating dynamic arrays
+    //creating pointers for dynamic array
     int *testScores;
     string *studentNames;
-    testScores = new int[CAPACITY];
-    studentNames = new string[CAPACITY];
 
     //declaring variables
     int maxTestScore;
     int studentRosterNumber;
     int holdTestScore;
 
-    //program
+    //asking for number of students in the class
     cout << "How many students were in the class?\n";
     cin >> studentRosterNumber;
     while(studentRosterNumber < 1 || studentRosterNumber > 100)
@@ -25,6 +23,10 @@ int main()
         cout << "You must enter a number between 1 and 100, try again.\n" << "How many students were in the class?\n";
         cin >> studentRosterNumber;
     }
+
+    //generating an array based on user input for the number of students
+    testScores = new int[studentRosterNumber];
+    studentNames = new string[studentRosterNumber];
 
     cout << "What is the maximum test score?\n";
     cin >> maxTestScore;
@@ -37,18 +39,21 @@ int main()
     for (int i = 0; i < studentRosterNumber; i++)
     {
         cout << "Enter the student's Name: \n";
-        cin >> *studentNames++;
+        cin >> studentNames[i];
 
         cout << "Enter the student's score: \n";
         cin >> holdTestScore;
         while(holdTestScore < 1 || holdTestScore > maxTestScore) { cout << "Error: You entered a score less than 1 or greater than " << maxTestScore << "\nEnter the student's score: \n"; cin >> holdTestScore; }
-        *testScores = holdTestScore;
-        *testScores++;
+        testScores[i] = holdTestScore;
     }
 
+    //sorting algorithm
+    sortArray(testScores, studentRosterNumber);
+
+    //display
     for (int i = 0; i < studentRosterNumber; i++)
     {
-        cout << testScores[i] << " " << studentNames[i] << " ";
+        cout << *(testScores + i) << " " << *(studentNames + i) << " ";
     }
 
     //ending program
@@ -57,4 +62,22 @@ int main()
     testScores = NULL;
     studentNames = NULL;
     return 0;
+}
+
+void sortArray(int numbersToSort[], int arraySize){ // the bubble sort does not sort these names correctly
+
+    bool swap;
+    int temp;
+
+    do {
+        swap = false;
+        for (int i = 0; i < (arraySize - 1); i++) {
+            if (numbersToSort[i] < numbersToSort[i + 1]){
+                temp = numbersToSort[i];
+                numbersToSort[i] = numbersToSort[i + 1];
+                numbersToSort[i + 1] = temp;
+                swap = true;
+            }
+        }
+    } while (swap);
 }
