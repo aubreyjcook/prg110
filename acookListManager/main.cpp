d6c677031c6b1ff6de92222353d88deb7c5f8091
Aubrey Cook <aubreyjcook@gmail.com>
2016-05-06 20:07:03 -0500

    acceptable list manager program 05062016
    needs work

--- acookListManager/main.cpp
+++ acookListManager/main.cpp
@@ -2,36 +2,45 @@
 
 using namespace std;
 
-void whatDidUserPick(int userChoice); //for selecting the right function based on user input from the menu and sending them to the right function
-void addMembers(string firstNames[], string lastNames[], string phoneNumbers[], int ages[], string tempFirstNames[], string tempLastNames[], string tempPhoneNumbers[], int tempAges[]); //for dynamically expanding and allocating the names and data of a member
-void displayFunction(string firstNames[], string lastNames[], string phoneNumbers[], int ages[]); //for displaying the current list of members, it calls to functions to sort and display separately
-void sortList(); //sorting algorithm
-void displayList(string firstNames[], string lastNames[], string phoneNumbers[], int ages[]); //step through array and display
-void clearList(); //clears the array and starts from the beginning
+void addMembers      (string firstNames[], string lastNames[], string phoneNumbers[], int ages[], string tempFirstNames[], string tempLastNames[], string tempPhoneNumbers[], int tempAges[]);
+//for dynamically expanding and allocating the names and data of a member
+void displayFunction (string firstNames[], string lastNames[], string phoneNumbers[], int ages[]);
+//for displaying the current list of members, it calls to functions to sort and display separately
+void sortList        (string firstNames[], string lastNames[], string phoneNumbers[], int ages[]);
+//sorting algorithm
+void displayList     (string firstNames[], string lastNames[], string phoneNumbers[], int ages[]);
+//step through array and display
+void clearList       (string firstNames[], string lastNames[], string phoneNumbers[], int ages[]);
+//clears the array and starts from the beginning
 
-int arraySize = 0; //global variable containing the number of array members
+int arraySize = 0;
+//global variable containing the number of array elements that have been assigned, it increments every time the add member function is run and resets when the clear option is selected
+const int ARRAYMAX = 10;
 
 int main()
 {
     //declaring pointers, each must stand for a parallel array of member data
-    string* firstNames         = new string[10];
-    string* lastNames          = new string[10];
-    string* phoneNumbers       = new string[10];
-    int*    ages               = new int[10];
+    string* firstNames         = new string[ARRAYMAX];
+    string* lastNames          = new string[ARRAYMAX];
+    string* phoneNumbers       = new string[ARRAYMAX];
+    int*    ages               = new int[ARRAYMAX];
 
-    string* tempFirstNames     = new string[1];
-    string* tempLastNames      = new string[1];
-    string* tempPhoneNumbers   = new string[1];
-    int* tempAges              = new int[1];
+    string* tempFirstNames     = new string[0];
+    string* tempLastNames      = new string[0];
+    string* tempPhoneNumbers   = new string[0];
+    int* tempAges              = new int[0];
 
 	//declaring variables
 	int userChoice;
 
     //array initialization
-    *(firstNames + arraySize)   = {"null"};
-    *(lastNames + arraySize)    = {"null"};
-    *(phoneNumbers + arraySize) = {"null"};
-    *(ages + arraySize)         = {0};
+    for(int i = 0; i < ARRAYMAX; i++)
+    {
+        *(firstNames + i)   = "zzzz";
+        *(lastNames + i)    = "zzzz";
+        *(phoneNumbers + i) = "zzzz";
+        *(ages + i)         = 5555;
+    }
 
 	do //this do-while statement controls for the exit functionality of the program
 	{
@@ -64,7 +73,7 @@ int main()
                 displayFunction(firstNames, lastNames, phoneNumbers, ages);
                 break;
             case 3:
-                clearList();
+                clearList(firstNames, lastNames, phoneNumbers, ages);
                 break;
             case 4:
                 break;
@@ -72,14 +81,14 @@ int main()
 	} while (userChoice != 4);
 
 	//closing program
-	delete [] firstNames;
-	delete [] lastNames;
-	delete [] phoneNumbers;
-	delete [] ages;
-	firstNames = NULL;
-	lastNames = NULL;
-	phoneNumbers = NULL;
-	ages = NULL;
+	delete [] firstNames;   delete [] tempFirstNames;
+	delete [] lastNames;    delete [] tempLastNames;
+	delete [] phoneNumbers; delete [] tempPhoneNumbers;
+	delete [] ages;         delete [] tempAges;
+	firstNames   = NULL; tempFirstNames   = NULL;
+	lastNames    = NULL; tempLastNames    = NULL;
+	phoneNumbers = NULL; tempPhoneNumbers = NULL;
+	ages         = NULL; tempAges         = NULL;
 	return 0;
 }
 
@@ -104,38 +113,43 @@ void addMembers(string firstNames[], string lastNames[], string phoneNumbers[], 
     cout << "Enter the Member's Age: \n";
     cin  >> newMemberAge;
 
-    if (arraySize > 1) //reallocating the arrays
+    if (arraySize > 1) //copy array operation from original array to temporary array then back to the original
     {
-
-        tempFirstNames   = new string[arraySize];
-        tempLastNames    = new string[arraySize];
-        tempPhoneNumbers = new string[arraySize];
-        tempAges         = new int[arraySize];
+        //clearing the temp array and rebuilding so it can store the temporary elements
+        tempFirstNames   = NULL; tempFirstNames   = new string[arraySize];
+        tempLastNames    = NULL; tempLastNames    = new string[arraySize];
+        tempPhoneNumbers = NULL; tempPhoneNumbers = new string[arraySize];
+        tempAges         = NULL; tempAges         = new int[arraySize];
 
         for(int i = 0; i < arraySize - 1; i++)
         {
-            *(tempFirstNames + i) = firstNames[i];
-            *(tempLastNames + i) = lastNames[i];
+            //copying into temporary array
+            *(tempFirstNames + i)   = firstNames[i];
+            *(tempLastNames + i)    = lastNames[i];
             *(tempPhoneNumbers + i) = phoneNumbers[i];
-            *(tempAges + i) = ages[i];
+            *(tempAges + i)         = ages[i];
         }
 
-        delete          [] firstNames;
-        delete          [] lastNames;
-        delete          [] phoneNumbers;
-        delete          [] ages;
+        /* this section of code would normally be how the original array is cleared and expanded, but it does not extend beyond the scope of this function
+        firstNames   = NULL; firstNames   = new string[arraySize];
+        lastNames    = NULL; lastNames    = new string[arraySize];
+        phoneNumbers = NULL; phoneNumbers = new string[arraySize];
+        ages         = NULL; ages         = new int[arraySize];
+        */
 
-        // This section of code is useless because it does not extend beyond the scope of this function, this makes it impossible to create a function that assigns a new array in this manner.
-        firstNames       = new string[arraySize + 1];
-        lastNames        = new string[arraySize + 1];
-        phoneNumbers     = new string[arraySize + 1];
-        ages             = new int[arraySize + 1];
+        for(int i = 0; i < arraySize - 1; i++)
+        {
+            //copying into the true array
+            *(firstNames + i)       = tempFirstNames[i];
+            *(lastNames + i)        = tempLastNames[i];
+            *(phoneNumbers + i)     = tempPhoneNumbers[i];
+            *(ages + i)             = tempAges[i];
+        }
 
-        firstNames       = tempFirstNames;
-        lastNames        = tempLastNames;
-        phoneNumbers     = tempPhoneNumbers;
-        ages             = tempAges;
-
+        delete [] tempFirstNames;
+        delete [] tempLastNames;
+        delete [] tempPhoneNumbers;
+        delete [] tempAges;
     }
 
     //adding the data to the new array
@@ -149,12 +163,30 @@ void addMembers(string firstNames[], string lastNames[], string phoneNumbers[], 
 
 void displayFunction(string firstNames[], string lastNames[], string phoneNumbers[], int ages[])
 {
-    sortList();
+    sortList(firstNames, lastNames, phoneNumbers, ages);
     displayList(firstNames, lastNames, phoneNumbers, ages);
 }
 
-void sortList()
+void sortList(string firstNames[], string lastNames[], string phoneNumbers[], int ages[])
 {
+    bool swap;
+    string swapFirstNames;
+    string swapLastNames;
+    string swapPhoneNumbers;
+    int swapAges;
+
+    do { //sort the last names, parallel sorting
+        swap = false;
+        for (int i = 0; i < (arraySize - 1); i++) {
+            if (lastNames[i] > lastNames[i + 1]){
+                swapLastNames = lastNames[i]; swapFirstNames = firstNames[i]; swapPhoneNumbers = phoneNumbers[i]; swapAges = ages[i];
+                lastNames[i] = lastNames[i + 1]; firstNames[i] = firstNames[i + 1]; phoneNumbers[i] = phoneNumbers[i + 1]; ages[i] = ages[i + 1];
+                lastNames[i + 1] = swapLastNames; firstNames[i + 1] = swapFirstNames; phoneNumbers[i + 1] = swapPhoneNumbers; ages[i + 1] = swapAges;
+                swap = true;
+            }
+        }
+    } while (swap);
+
 
 }
 
@@ -168,7 +200,33 @@ void displayList(string firstNames[], string lastNames[], string phoneNumbers[],
     } else {cout << "Nothing to display." << endl;}
 }
 
-void clearList()
+void clearList(string firstNames[], string lastNames[], string phoneNumbers[], int ages[])
 {
+    char startOver;
 
+    cout << "Delete all entries and start over? y/n\n";
+    cin >> startOver;
+
+    while(startOver != 'y' && startOver != 'n' && startOver != 'Y' && startOver != 'N')
+    {
+        cout << "Delete all entries and start over? y/n\n";
+        cin >> startOver;
+    }
+
+    if (startOver == 'y' || startOver == 'Y')
+    {
+        cout << "Okay. Erasing array.\n";
+
+        for(int i = 0; i < ARRAYMAX; i++)
+        {
+            *(firstNames + i)   = "zzzz";
+            *(lastNames + i)    = "zzzz";
+            *(phoneNumbers + i) = "zzzz";
+            *(ages + i)         = 5555;
+        }
+
+        arraySize = 0;
+
+        cout << "Array erased." << endl;
+    }
 }
